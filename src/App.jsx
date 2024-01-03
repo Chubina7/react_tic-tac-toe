8c6e1b0f2261b5d0130782d55a1e2d2939342200
Luka Chubinidze <chubinidzel08@gmail.com>
2024-01-03 11:44:44 +0400

    structured files; lifted up state

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Board from "./components/board/Board";
+
+function App() {
+  return (
+    <main className="App">
+      <Board />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/board/Board.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import "./Board.css";
+import Square from "../square/Square";
+
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const squareClickHandler = (i) => {
+    const newSquareValue = squares.slice();
+    newSquareValue[i] = "X";
+    setSquares(newSquareValue);
+  };
+
+  return (
+    <div className="board">
+      <div className="row">
+        <Square
+          value={squares[0]}
+          onSquareClick={() => {
+            squareClickHandler(0);
+          }}
+        />
+        <Square
+          value={squares[1]}
+          onSquareClick={() => {
+            squareClickHandler(1);
+          }}
+        />
+        <Square
+          value={squares[2]}
+          onSquareClick={() => {
+            squareClickHandler(2);
+          }}
+        />
+      </div>
+      <div className="row">
+        <Square
+          value={squares[3]}
+          onSquareClick={() => {
+            squareClickHandler(3);
+          }}
+        />
+        <Square
+          value={squares[4]}
+          onSquareClick={() => {
+            squareClickHandler(4);
+          }}
+        />
+        <Square
+          value={squares[5]}
+          onSquareClick={() => {
+            squareClickHandler(5);
+          }}
+        />
+      </div>
+      <div className="row">
+        <Square
+          value={squares[6]}
+          onSquareClick={() => {
+            squareClickHandler(6);
+          }}
+        />
+        <Square
+          value={squares[7]}
+          onSquareClick={() => {
+            squareClickHandler(7);
+          }}
+        />
+        <Square
+          value={squares[8]}
+          onSquareClick={() => {
+            squareClickHandler(8);
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Board;
